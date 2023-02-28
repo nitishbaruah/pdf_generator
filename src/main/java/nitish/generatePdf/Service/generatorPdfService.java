9080f180c9b0c6a7f7972f2ded553bb54306ed7a
nitish <diluboruah@gmail.com>
2023-02-28 15:03:45 +0530

    Pdf generated

--- src/main/java/nitish/generatePdf/Service/generatorPdfService.java
+++ src/main/java/nitish/generatePdf/Service/generatorPdfService.java
@@ -1,14 +1,19 @@
 package nitish.generatePdf.Service;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.Table;
 import nitish.generatePdf.Models.BuyerSeller;
+import nitish.generatePdf.Models.item;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 @Service
@@ -17,12 +22,38 @@ public class generatorPdfService {
     public ByteArrayInputStream generatePdf(BuyerSeller data) throws Exception{
         //logger.info("Creating");
         ByteArrayOutputStream out=new ByteArrayOutputStream();
-        Document doc= new Document();
-        PdfWriter.getInstance(doc,out);
-        String title="hello";
-        doc.open();
-        Paragraph paragraph=new Paragraph(title);
-        doc.add(paragraph);
+        PdfWriter writer = new PdfWriter(out);
+        PdfDocument pdfDoc = new PdfDocument(writer);
+        Document doc = new Document(pdfDoc);
+        // String title="hello";
+
+        //Paragraph paragraph=new Paragraph(title);
+        //doc.add(paragraph);
+        float [] pointColumnWidths = {100F, 100F, 100F,100F};
+        Table table = new Table(pointColumnWidths);
+        table.addCell(new Cell(0,2).add("Seller:\n"+
+                                                        data.getSeller()+"\n" +
+                                                        data.getSellerAddress()+"\n"+
+                                                        "GSTIN"+data.getSellerGstin()));
+        table.addCell(new Cell(0,2).add("Buyer:\n"+
+                data.getBuyer()+"\n" +
+                data.getBuyerAddress()+"\n"+
+                "GSTIN"+data.getBuyerGstin()));
+        table.addCell(new Cell().add("Item"));
+        table.addCell(new Cell().add("Quantity"));
+        table.addCell(new Cell().add("Rate"));
+        table.addCell(new Cell().add("Amount"));
+        List<item> itemsList=data.getItems();
+        for(int i=0;i<itemsList.size();i++){
+            table.addCell(new Cell().add(itemsList.get(i).getName()));
+            table.addCell(new Cell().add(itemsList.get(i).getQuantity()));
+            table.addCell(new Cell().add(itemsList.get(i).getName()));
+            table.addCell(new Cell().add(itemsList.get(i).getName()));
+        }
+        table.addCell(new Cell(0,4).add("\n"+"\n"+"\n"+"\n"));
+
+        // Adding Table to document
+        doc.add(table);
         doc.close();
         return new ByteArrayInputStream(out.toByteArray());
     }
