ca9bdc34a7fe5b2c2c49604f0e63ac9b3dcceec0
nitish <diluboruah@gmail.com>
2023-03-01 07:32:50 +0530

    formatted

--- src/main/java/nitish/generatePdf/Service/generatorPdfService.java
+++ src/main/java/nitish/generatePdf/Service/generatorPdfService.java
@@ -5,6 +5,7 @@ import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.property.TextAlignment;
 import nitish.generatePdf.Models.BuyerSeller;
 import nitish.generatePdf.Models.item;
 import org.slf4j.LoggerFactory;
@@ -29,26 +30,28 @@ public class generatorPdfService {
 
         //Paragraph paragraph=new Paragraph(title);
         //doc.add(paragraph);
-        float [] pointColumnWidths = {100F, 100F, 100F,100F};
+        float [] pointColumnWidths = {150F, 150F, 150F,150F};
         Table table = new Table(pointColumnWidths);
         table.addCell(new Cell(0,2).add("Seller:\n"+
                                                         data.getSeller()+"\n" +
                                                         data.getSellerAddress()+"\n"+
-                                                        "GSTIN"+data.getSellerGstin()));
+                                                        "GSTIN: "+data.getSellerGstin()).setMargins(10F,10F,10F,25F));
         table.addCell(new Cell(0,2).add("Buyer:\n"+
                 data.getBuyer()+"\n" +
                 data.getBuyerAddress()+"\n"+
-                "GSTIN"+data.getBuyerGstin()));
-        table.addCell(new Cell().add("Item"));
-        table.addCell(new Cell().add("Quantity"));
-        table.addCell(new Cell().add("Rate"));
-        table.addCell(new Cell().add("Amount"));
+                "GSTIN: "+data.getBuyerGstin()).setMargins(10F,10F,10F,25F));
+        table.addCell(new Cell().add("Item").setTextAlignment(TextAlignment.CENTER));
+        table.addCell(new Cell().add("Quantity").setTextAlignment(TextAlignment.CENTER));
+        table.addCell(new Cell().add("Rate").setTextAlignment(TextAlignment.CENTER));
+        table.addCell(new Cell().add("Amount").setTextAlignment(TextAlignment.CENTER));
         List<item> itemsList=data.getItems();
         for(int i=0;i<itemsList.size();i++){
-            table.addCell(new Cell().add(itemsList.get(i).getName()));
-            table.addCell(new Cell().add(itemsList.get(i).getQuantity()));
-            table.addCell(new Cell().add(itemsList.get(i).getName()));
-            table.addCell(new Cell().add(itemsList.get(i).getName()));
+            String rate=String.format("%.2f",itemsList.get(i).getRate());
+            String amount=String.format("%.2f",itemsList.get(i).getAmount());
+            table.addCell(new Cell().add(itemsList.get(i).getName()).setTextAlignment(TextAlignment.CENTER));
+            table.addCell(new Cell().add(itemsList.get(i).getQuantity()).setTextAlignment(TextAlignment.CENTER));
+            table.addCell(new Cell().add(rate).setTextAlignment(TextAlignment.CENTER));
+            table.addCell(new Cell().add(amount).setTextAlignment(TextAlignment.CENTER));
         }
         table.addCell(new Cell(0,4).add("\n"+"\n"+"\n"+"\n"));
 
